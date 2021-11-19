20cf1626cd3fafbd0d201b8bc94c0ecddfca20df
PedroJSDev <pjsoares46@gmail.com>
2021-11-19 17:18:58 -0300

    update

--- src/routes/Fastdelivery.svelte
+++ src/routes/Fastdelivery.svelte
@@ -1,1 +1,18 @@
-<h3>Pagina de pronta entrega!</h3>
+<script>
+	import {getDatabase} from '../notion'
+  let data = []
+
+  async function getList(){
+    let list = await getDatabase()
+    data = list
+  }
+
+</script>
+<h3>Fast Delivery page</h3>
+<button on:click="{()=> getList()}">Obter Lista</button>
+
+{#each data as item}
+
+  <li>{item}hahah</li>
+
+{/each}
